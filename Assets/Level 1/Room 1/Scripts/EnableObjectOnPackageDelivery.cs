a7e3dbc3eee5c215a61ddc158d13f6dd8338ce89
Cameron <1029194@student.sae.edu.au>
2023-11-23 10:12:15 +0800

    Push for test
    Made Mildred less creepy

--- Assets/Level 1/Room 1/Scripts/EnableObjectOnPackageDelivery.cs
+++ Assets/Level 1/Room 1/Scripts/EnableObjectOnPackageDelivery.cs
@@ -14,22 +14,12 @@ public class EnableObjectOnPackageDelivery : MonoBehaviour {
     private void OnTriggerEnter(Collider other) {
         if (isCatCollarPackage && other.gameObject.name == "CatCollar_Collider") {
             catCollar.SetActive(true);
-            npc.InitSingularDialogue(npc.singularDialogue[2]); // The Old Lady will thank you for delivering the Cat Collar
+            npc.InitSingularDialogue(npc.singularDialogue[0]); // The Old Lady will thank you for delivering the Cat Collar
         }
 
         else if (!isCatCollarPackage && other.gameObject.name == "CatFood_Collider") {
             catFood.SetActive(true);
-            npc.InitSingularDialogue(npc.singularDialogue[3]); // The Old Lady will thank you for delivering the Cat Food
-        }
-
-        else if (isCatCollarPackage && other.gameObject.name == "OldLadyDialogueTrigger_EnterApartment" && !enteredTheApartment) {
-            npc.InitSingularDialogue(npc.singularDialogue[0]); // The Old Lady will give you directions for delivering the Cat Collar
-            enteredTheApartment = true;
-        }
-
-        else if (!isCatCollarPackage && other.gameObject.name == "OldLadyDialogueTrigger_EnterApartment" && !enteredTheApartment) {
-            npc.InitSingularDialogue(npc.singularDialogue[1]); // The Old Lady will give you directions for delivering the Cat Food
-            enteredTheApartment = true;
+            npc.InitSingularDialogue(npc.singularDialogue[1]); // The Old Lady will thank you for delivering the Cat Food
         }
     }
 }
--- Assets/Level 1/Room 1/Scripts/LookAtPlayerOnActiveDialogue.cs
+++ Assets/Level 1/Room 1/Scripts/LookAtPlayerOnActiveDialogue.cs
@@ -5,20 +5,30 @@ public class LookAtPlayerOnActiveDialogue : MonoBehaviour {
     private Animator animator;
     [SerializeField] private Camera playerCamera;
 
+    [SerializeField] private float distanceToPlayer;
+    [SerializeField] private float lookAtWeight = 0;
+
     public bool ikActive = false;
 
     private void Awake() {
         animator = GetComponent<Animator>();
     }
 
+    private void Update() {
+        distanceToPlayer = Vector3.Distance(transform.position, playerCamera.transform.position);
+    }
+
     private void OnAnimatorIK() {
-        if(ikActive) {
-            animator.SetLookAtWeight(1);
+        if(ikActive && distanceToPlayer <= 3f) {
+            animator.SetLookAtWeight(lookAtWeight += Time.deltaTime);
             animator.SetLookAtPosition(playerCamera.transform.position);
         }
 
         else {
-            animator.SetLookAtWeight(0);
+            animator.SetLookAtWeight(lookAtWeight -= Time.deltaTime);
+
+            if (lookAtWeight < 0)
+                lookAtWeight = 0;
         }
     }
 }
