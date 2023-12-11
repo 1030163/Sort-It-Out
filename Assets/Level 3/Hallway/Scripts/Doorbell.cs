d864d8907e3718e9c1f17eab95a283e5f7bf7053
mluc <1030581@student.sae.edu.au>
2023-12-11 13:28:38 +0800

    Doorbell + Day2 stuff

--- Assets/Level 3/Hallway/Scripts/Doorbell.cs
+++ Assets/Level 3/Hallway/Scripts/Doorbell.cs
@@ -6,63 +6,77 @@ using UnityEngine.SceneManagement;
 
 public class Doorbell : MonoBehaviour
 {
-    [SerializeField] TextMeshPro interactionText;
-    [SerializeField] GameObject door;
-    [SerializeField] PackageLocation packageDelivery;
-    [SerializeField] GameObject mrsFlower;
-    [SerializeField] Vector3 mrsFlowerNewPosition = new Vector3(39.0f, 0.0f, 50.5f);
+    [Header("Changeable Values")]
+    [SerializeField] float waitTime;
 
+    [Header("References")]
+    [SerializeField] TextMeshPro interactionText;
+    [SerializeField] GameObject doorGameObject;
+    [SerializeField] AudioClip doorbellSound;
+    private AudioSource audioSource;
+    public bool isDoorOpen;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>(); 
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        interactionText.gameObject.SetActive(true);
-        if (packageDelivery.isCorrectPackage)
+        if(other.CompareTag("Player"))
         {
-            WaitCoroutine(5f);
-            door.SetActive(true);
-
+            interactionText.gameObject.SetActive(true);
         }
 
-        MoveMrsFlower();
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (packageDelivery.isCorrectPackage)
+        if (other.CompareTag("Player") && Input.GetMouseButtonDown(0))
         {
-            WaitCoroutine(5f);
-            door.SetActive(true);
-
+            RingDoorbell();
         }
-
-        if (SceneManager.GetActiveScene().buildIndex == 6)
-        {
-            MoveMrsFlower();
-        }
-
-        MoveMrsFlower();
-
 
     }
 
     private void OnTriggerExit(Collider other)
     {
-        interactionText.gameObject.SetActive(false);
-        MoveMrsFlower();
+        if (other.CompareTag("Player"))
+        {
+            interactionText.gameObject.SetActive(false);
+            isDoorOpen = false;
+        }
     }
 
-    public void RingDoorbell ()
+    public void RingDoorbell()
     {
-        WaitCoroutine(2f);
-        door.SetActive(false);
+        StartCoroutine(WaitCoroutine(waitTime));
+        OpenDoor();
+        PlayDoorbellSound(); 
     }
 
-    private void MoveMrsFlower()
-    {
-        mrsFlower.transform.position = mrsFlowerNewPosition;
-    }
     IEnumerator WaitCoroutine(float time)
     {
         yield return new WaitForSeconds(time);
+
+
+    }
+
+    private void OpenDoor()
+    {
+        // Check if the player is still in the interaction zone before opening the door
+        if (interactionText.gameObject.activeSelf)
+        {
+            doorGameObject.SetActive(false);
+            isDoorOpen= true;
+        }
+    }
+
+    private void PlayDoorbellSound()
+    {
+        if (doorbellSound != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(doorbellSound);
+        }
     }
 }
---
+++ Assets/Level 3/Room 1/Scripts/Day2FamilyManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Day2FamilyManager : MonoBehaviour
+{
+    [SerializeField] GameObject npc1;//MrsFlowers at door
+    [SerializeField] GameObject npc2; //MrsFlowers at kitchen
+    [SerializeField] NPCDialogueHandler npc1DialogueHandler; 
+    [SerializeField] NPCDialogueHandler npc2DialogueHandler; 
+    [SerializeField] Doorbell doorbell;
+    [SerializeField] PackageLocation package1Location; //package
+    [SerializeField] PackageLocation package2Location; //phone
+
+
+    private bool dialogue1Init;
+    private bool dialogue2Init;
+    private bool dialogue3Init;
+
+    private void Start()
+    {
+        dialogue1Init = false;
+        dialogue2Init = false;
+        dialogue3Init = false;
+    }
+    private void Update()
+    {
+        if (doorbell.isDoorOpen && dialogue1Init == false)
+        {
+            npc1DialogueHandler.InitSingularDialogue(npc1DialogueHandler.singularDialogue[0]); // 1.Mrs Flowers answers the door
+            dialogue1Init= true;
+        }
+
+        if (package1Location.isCorrectPackage)
+        {
+            npc2DialogueHandler.InitSingularDialogue(npc2DialogueHandler.singularDialogue[1]); // 3.Mrs Flowers asks you to look for her phone
+            dialogue2Init = true;
+        }
+
+        if (package2Location.isCorrectPackage)
+        {
+            npc2DialogueHandler.InitSingularDialogue(npc2DialogueHandler.singularDialogue[2]); // 4.Mrs Flowers thanks you
+            dialogue3Init = true;
+        }
+
+    }
+
+    public void Dialogue1Confirm()
+    {
+        npc1.SetActive(false); 
+        npc2.SetActive(true);
+        npc2DialogueHandler.InitSingularDialogue(npc2DialogueHandler.singularDialogue[0]); // 2.Mrs Flowers tells player to put package on the table
+    }
+    
+}
