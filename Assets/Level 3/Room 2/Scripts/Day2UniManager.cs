0a2a806c35b99883b2fc333cd30f8220c126a026
mluc <1030581@student.sae.edu.au>
2023-12-14 17:58:09 +0800

    Dialogue complete!

--- Assets/Level 3/Room 2/Scripts/Day2UniManager.cs
+++ Assets/Level 3/Room 2/Scripts/Day2UniManager.cs
@@ -6,7 +6,6 @@ using UnityEngine.Rendering;
 public class Day2UniManager : MonoBehaviour
 {
     [SerializeField] GameObject npc1; //Sam at door
-    [SerializeField] GameObject npc2; //Sam in room
     //[SerializeField] NPCDialogueHandler npc1DialogueHandler;
     //[SerializeField] NPCDialogueHandler npc2DialogueHandler;
     [SerializeField] Doorbell doorbell;
@@ -14,15 +13,24 @@ public class Day2UniManager : MonoBehaviour
     [SerializeField] DialogueHandler npc1DialogueHandler;
     [SerializeField] DialogueTree[] dialogueTree;
     [SerializeField] NPCDialogue[] singularDialogue;
+    [SerializeField] Transform samNewPosition;
+    [SerializeField] DoorController door2;
+    int keyPress;
 
 
     private bool dialogue1Init;
     private bool dialogue2Init;
+    private bool dialogue3Init;
+    private bool dialogue4Init;
+    private bool dialogue5Init;
 
     private void Start()
     {
         dialogue1Init = false;
         dialogue2Init = false;
+        dialogue3Init = false;
+        dialogue4Init = false;
+        dialogue5Init = false;
     }
     private void Update()
     {
@@ -31,23 +39,26 @@ public class Day2UniManager : MonoBehaviour
             npc1DialogueHandler.InitSingularDialogue(singularDialogue[0], "Sam Andre", VOICE_TYPE.Masculine);// 1.Sam answers the door
             dialogue1Init = true;
         }
-
-        if (package1Location.isCorrectPackage)
+        if (dialogue1Init = true && Input.GetKeyUp(KeyCode.E))
+            {
+            keyPress++;
+        }
+        if (dialogue2Init == false && keyPress == 2)
         {
-            npc1DialogueHandler.InitSingularDialogue(singularDialogue[2], "Sam Andre", VOICE_TYPE.Masculine); // 3.Sam cheers
+            npc1.transform.position = samNewPosition.position;
+            door2.DoorStateChange();
+            npc1DialogueHandler.InitDialogueTree(dialogueTree[0], "Sam Andre", VOICE_TYPE.Masculine);// 2.Sam shows the basketball hoop - instructions - hold down left mouse button to throw
             dialogue2Init = true;
         }
 
-    }
+        
 
-    public void Dialogue1Confirm1()
-    {
-        npc1DialogueHandler.InitSingularDialogue(singularDialogue[1], "Sam Andre", VOICE_TYPE.Masculine);// 2.Sam shows the basketball hoop - instructions - hold down left mouse button to throw
-    }
+        if (package1Location.isCorrectPackage && dialogue3Init == false)
+        {
+            npc1DialogueHandler.InitDialogueTree(dialogueTree[1], "Sam Andre", VOICE_TYPE.Masculine); // 3.Sam cheers and player tells about mr flowers
+            dialogue3Init = true;
+        }
 
-    public void Dialogue1Confirm2()
-    {
-        npc1DialogueHandler.InitSingularDialogue(singularDialogue[3], "Sam Andre", VOICE_TYPE.Masculine);// 4. Sam laments about Mr Flowers
     }
 
 }
