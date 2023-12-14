6893c669a0a4afc9028db3b88500b8b872ee1d85
Woody 1024800 <1024800@student.sae.edu.au>
2023-12-14 19:00:55 +0800

    FINAL PUSH Floor 2

---
+++ Assets/Level 2/Room 1/Scripts/Floor2Day1Manager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor2Day1Manager : MonoBehaviour
+{
+
+    [SerializeField] DialogueHandler dialogueHandler;
+
+    public DialogueTree[] dialogueTrees;
+    public NPCDialogue[] singularDialogue;
+    [SerializeField] Doorbell doorbell;
+    [SerializeField] Doorbell toneysDoorbell;
+    [SerializeField] PackageLocation packageLocation1;
+    [SerializeField] PackageLocation packageLocation2;
+
+    bool bMVaseDelivered;
+    bool metWesley;
+    bool wesleyPaintsDelivered;
+    bool metToney;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doorbell.isDoorOpen && !metWesley)
+        {
+            dialogueHandler.InitDialogueTree(dialogueTrees[0], "Wesley Stashwell", VOICE_TYPE.Masculine); //dialogue for door open
+            metWesley = true;
+        }
+
+        if (toneysDoorbell.isDoorOpen && !metToney)
+        {
+            dialogueHandler.InitDialogueTree(dialogueTrees[1], "Toney Immaculate", VOICE_TYPE.Masculine); //dialogue for package delivered
+            metToney = true;
+        }
+        if (packageLocation2.isCorrectPackage && !wesleyPaintsDelivered)
+        {
+            dialogueHandler.InitDialogueTree(dialogueTrees[2], "Wesley Stashwell", VOICE_TYPE.Masculine); //dialogue for package delivered
+            wesleyPaintsDelivered = true;
+        }
+        if (packageLocation1.isCorrectPackage && !bMVaseDelivered)
+        {
+            dialogueHandler.InitDialogueTree(dialogueTrees[3], "Toney Immaculate", VOICE_TYPE.Masculine); //dialogue for package delivered
+            bMVaseDelivered = true;
+        }
+
+    }
+}
