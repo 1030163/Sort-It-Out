e1c145e76e7b23f833daf9c5119cb7f96e184e43
Cameron <1029194@student.sae.edu.au>
2023-12-14 13:53:32 +0800

    fixed floor 1 dialogue trigger issue
    now changing scenes wont reset the trigger to call dialogue

--- Assets/Level 1/Room 1/Scripts/CallDialogueOnTrigger.cs
+++ Assets/Level 1/Room 1/Scripts/CallDialogueOnTrigger.cs
@@ -5,8 +5,11 @@ public class CallDialogueOnTrigger : MonoBehaviour {
     [SerializeField] private DialogueHandler dialogueHandler;
     [SerializeField] private DialogueTree Tree_EnterApartment;
 
+    static bool hasDialoguePlayed = false;
+
     private void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Player")) {
+        if (other.CompareTag("Player") && !hasDialoguePlayed) {
+            hasDialoguePlayed = true;
             GetComponent<BoxCollider>().enabled = false;
             dialogueHandler.InitDialogueTree(Tree_EnterApartment, "Mildred Brown", VOICE_TYPE.Feminine);
         }
