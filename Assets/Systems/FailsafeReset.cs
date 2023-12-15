4f4dc1f7cd35ea53d7f61fcebbe733e20f1b4229
Max Maley <1030163@student.sae.edu.au>
2023-12-15 10:05:40 +0800

    Showcase bbuild
    some glaring problems remain

--- Assets/Systems/FailsafeReset.cs
+++ Assets/Systems/FailsafeReset.cs
@@ -19,5 +19,37 @@ public class FailsafeReset : MonoBehaviour
 
             UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name, UnityEngine.SceneManagement.LoadSceneMode.Single);
         }
+
+        if (Input.GetKey(KeyCode.P))
+        {
+            if (Input.GetKey(KeyCode.Alpha0))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(0, UnityEngine.SceneManagement.LoadSceneMode.Single);
+            }
+            if (Input.GetKey(KeyCode.Alpha1))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(1, UnityEngine.SceneManagement.LoadSceneMode.Single);
+            }
+            if (Input.GetKey(KeyCode.Alpha2))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(2, UnityEngine.SceneManagement.LoadSceneMode.Single);
+            }
+            if (Input.GetKey(KeyCode.Alpha3))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(3, UnityEngine.SceneManagement.LoadSceneMode.Single);
+            }
+            if (Input.GetKey(KeyCode.Alpha4))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(4, UnityEngine.SceneManagement.LoadSceneMode.Single);
+            }
+            if (Input.GetKey(KeyCode.Alpha5))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(5, UnityEngine.SceneManagement.LoadSceneMode.Single);
+            }
+            if (Input.GetKey(KeyCode.Alpha6))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(6, UnityEngine.SceneManagement.LoadSceneMode.Single);
+            }
+        }
     }
 }
